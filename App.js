e36afc13703cedd8a12dedaddf236936e871bd4f
sameerasad <ssameerkhan525@gmail.com>
2020-11-24 19:03:46 +0500

    flex-box basics

--- App.js
+++ App.js
@@ -1,41 +1,40 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Alert,
-  Image,
-  TouchableHighlight,
-} from "react-native";
-import {
-  useDimensions,
-  useDeviceOrientation,
-} from "@react-native-community/hooks";
+import { View, Text } from "react-native";
 
 export default function App() {
-  console.log(useDeviceOrientation()); //landscape true and portrait false
-  const { landscape } = useDeviceOrientation(); //destructing to get just landscape
   return (
-    <View
-      style={styles.container}
-      style={{
-        width: "100%",
-        height: landscape ? "30%" : "100%",
-        backgroundColor: "dodgerblue",
-      }}
-    >
-      Hello WORLD!
+    <View style={{ flex: 1, backgroundColor: "white" }}>
+      <View
+        style={{
+          flex: 0.3,
+          backgroundColor: "dodgerblue",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          style={{
+            paddingTop: "20px",
+            flex: 1,
+
+            backgroundColor: "red",
+          }}
+        >
+          {" "}
+          hello world
+        </Text>
+      </View>
+      <View style={{ flex: 1, backgroundColor: "orange" }} />
+      <View style={{ flex: 1, backgroundColor: "tomato" }} />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
+/*const styles = StyleSheet.create({
   container: {
     flex: "1",
     alignItems: "center",
     justifyContent: "center",
     backgroundColor: "#fff",
   },
-});
+});*/
